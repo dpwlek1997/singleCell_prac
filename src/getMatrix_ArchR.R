09ba66fa3e7da84eeb51df7a5f6a081f1e349ee4
Yeji Bae <dpwlek1997@gmail.com>
2023-07-21 16:21:51 -0700

    test

---
+++ src/getMatrix_ArchR.R
@@ -0,0 +1,7 @@
+# Install and load packages
+devtools::install_github("swaruplabUCI/ArchRtoSignac"
+packages <- c("ArchRtoSignac", "ArchR","Seurat", "Signac","stringr") # required packages
+loadinglibrary(packages)
+
+# This script had been generated for ArchRtoSignac with more varaible Matrix not just peakMatrix and GeneScoreMatrix.
+# But other matrixes and motifMatrix. 
